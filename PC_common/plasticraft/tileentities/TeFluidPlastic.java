7dc7308884c59edd58fa7a4d5b364d8d1b780ceb
darkracer <stijnringeling1@gmail.com>
2013-12-28 18:25:20 +0100

    added randomnes to liquid plastic

--- PC_common/plasticraft/tileentities/TeFluidPlastic.java
+++ PC_common/plasticraft/tileentities/TeFluidPlastic.java
@@ -1,21 +1,29 @@
 package plasticraft.tileentities;
 
+import java.util.Random;
+
 import net.minecraft.tileentity.TileEntity;
+import plasticraft.PlastiCraft;
 import plasticraft.blocks.Blocks;
 
 public class TeFluidPlastic extends TileEntity{
 
 	private int counter;
+	private Random r;
+	private int time;
 	
 	public TeFluidPlastic(){
 		this.counter = 0;
+		this.r = new Random();
+		this.time = 200 + r.nextInt(201);
+		
 	}
 	@Override
 	public void updateEntity(){
 		if(!this.worldObj.isRemote){
 			if(this.worldObj.getBlockMetadata(this.xCoord, this.yCoord, this.zCoord) == 0){
 				this.counter++;
-				if(this.counter >= 200){
+				if(this.counter >= this.time){
 					this.worldObj.setBlock(this.xCoord, this.yCoord, this.zCoord, Blocks.BlockPlastic.blockID);
 				}
 			}
